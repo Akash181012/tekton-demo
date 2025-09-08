8f5806ae472c24602500ed053cd4845ed76c91bc
akash <kushi.akee@gmail.com>
2025-09-08 14:54:16 +0530

    editted

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello,It is me Tekton Python CI/CD!"
+    return "I am Tekton Python CI/CD!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
