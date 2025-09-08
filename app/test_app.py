a8493643b015135e90a589efef14ea153f1d5703
akash <kushi.akee@gmail.com>
2025-09-08 22:09:46 +0530

    editted

--- app/test_app.py
+++ app/test_app.py
@@ -4,4 +4,4 @@ def test_root():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Hello,I am from Tekton Python CI/CD!" in response.data
+    assert b"Hlo,I am from Tekton Python CI/CD!" in response.data
